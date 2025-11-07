9d0c5f8582388a2cf6e1e71d70ea8682da7da8bd
arboivin <arboivin@mun.ca>
2025-11-07 11:25:41 -0330

    modified:   delivery_restaurant.h 	modified:   restaurant.h

--- restaurant.h
+++ restaurant.h
@@ -8,11 +8,29 @@ using namespace std;
 class Restaurant {
 public:
     Restaurant();
+
+    Restaurant(const string& restaurantName, const string& yearEstablished) :
+        name(restaurantName),
+        year(yearEstablished)
+    {}
+
+    Restaurant(Restaurant& other) :
+        name(other.name),
+        year(other.year)
+    {}
+
+    Restaurant(Restaurant&& other) :
+        name(std::move(other.name)),
+        year(std::move(other.year))
+    {}
+
     string getName() { return name; }
+
     Restaurant setName(string n) {
         name = n;
         return *this;
     }
+
     string getYear() { return year; }
 
     virtual void print() {
