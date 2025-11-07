72a65c39abd9070e96a385710b54532fbe22f01d
arboivin <arboivin@mun.ca>
2025-11-07 10:59:17 -0330

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	deleted:    aaaa 	deleted:    aaaaa 	new file:   restaurant.h

---
+++ restaurant.h
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Restaurant {
+public:
+    Restaurant();
+    string getName() { return name; }
+    Restaurant setName(string n) {
+        name = n;
+        return *this;
+    }
+    string getYear() { return year; }
+
+    virtual void print() {
+        cout << "Name: " << name << "\nYear established: " << name << endl;
+    }
+
+protected:
+    string name;
+    string year;
+};
